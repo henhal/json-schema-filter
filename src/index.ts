8bf7cf1a0bed007cea52c3e0851f28071f7d5cc9
Henrik Hall <henrik.hall@sonymobile.com>
2021-02-17 22:09:02 +0100

    Refactor

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import {JSONSchema4} from 'json-schema';
+import {JSONSchema4, JSONSchema4TypeName} from 'json-schema';
 
 type FilteredItem = { path: string, reason: string };
 
@@ -8,6 +8,26 @@ class ValidationError extends Error {
   }
 }
 
+function getDataType(data: any): string {
+  if (Array.isArray(data)) return 'array';
+
+  if (data === null) return 'null';
+
+  return typeof data;
+  //
+  // const type = typeof data;
+  //
+  // switch (type) {
+  //   case 'number':
+  //   case 'object':
+  //   case 'string':
+  //   case 'boolean':
+  //     return type;
+  //
+  //   default:
+  //     return undefined;
+  // }
+}
 function toArray<T>(x: T | T[] | undefined): T[] | undefined {
   if (x === undefined) {
     return undefined;
@@ -36,166 +56,303 @@ export function filter(
     }
 
     // TODO turn this around to one loop of types
+    const dataType = getDataType(data);
 
-    if (data === undefined) {
-      throw new ValidationError(`Expected ${types} but found undefined`, path);
-    }
-
-    if (data === null) {
-      for (const type of types) {
-        if (type === 'null') {
-          return data;
-        }
-      }
-      throw new ValidationError(`Expected ${types} but found null`, path);
-    }
-
-    if (Array.isArray(data)) {
-      for (const type of types) {
-        if (type === 'array') {
-          const {items = true, additionalItems = true, minItems = 0, maxItems = Number.MAX_SAFE_INTEGER} = schema;
-
-          if (data.length < minItems) {
-            throw new ValidationError(`Array shorter than ${minItems}`, path);
+    for (const type of types) {
+      switch (type) {
+        case 'null':
+          if (dataType === 'null') {
+            return data;
           }
+          break;
 
-          return (data as any[]).reduce((result, v, i) => {
-            const subPath = `${path}[${i}]`;
-            const s = Array.isArray(items) ?
-                i < items.length ? items[i] : additionalItems :
-                items;
+        case 'array':
+          if (dataType === 'array') {
+            const {items = true, additionalItems = true, minItems = 0, maxItems = Number.MAX_SAFE_INTEGER} = schema;
 
-            if (s === false) {
-              if (!options.removeAdditionalItems) {
-                throw new ValidationError(`Array item outside of schema items and additionalItems is false`, subPath);
-              }
-              filtered.push({
-                path: subPath,
-                reason: `Array item outside of schema items and additionalItems is false`,
-              });
-            } else if (i >= maxItems) {
-              if (!options.cropArrays) {
-                throw new ValidationError(`Array longer than ${maxItems}`, path);
-              }
-
-              filtered.push({
-                path: subPath,
-                reason: `Array item index ${i} outside of schema maxItems ${maxItems}`,
-              });
-            } else if (s === true) {
-              result.push(v);
-            } else {
-              result.push(inner(v, s, subPath));
+            if (data.length < minItems) {
+              throw new ValidationError(`Array shorter than ${minItems}`, path);
             }
-            return result;
-          }, [] as any[]);
-        }
-      }
 
-      throw new ValidationError(`Expected ${types} but found array`, path);
-    }
+            return (data as any[]).reduce((result, v, i) => {
+              const subPath = `${path}[${i}]`;
+              const s = Array.isArray(items) ?
+                  i < items.length ? items[i] : additionalItems :
+                  items;
 
-    if (typeof data === 'object') {
-      for (const type of types) {
-        if (type === 'object') {
-          const {properties, additionalProperties = true, required = []} = schema;
-
-          if (!options.ignoreRequiredProperties && required) {
-            for (const k of required) {
-              if (data[k] === undefined) {
-                throw new ValidationError(`Required object property ${k} missing`, `${path}.${k}`);
-              }
-            }
-          }
-
-          return Object.entries(data)
-              .reduce((result, [k, v]) => {
-                if (v !== undefined) {
-                  const subPath = `${path}.${k}`;
-                  const s = properties?.[k] ?? additionalProperties;
-
-                  if (s === false) {
-                    if (!options.removeAdditionalProperties) {
-                      throw new ValidationError(`Object property not in schema properties and additionalProperties is false`, subPath);
-                    }
-                    filtered.push({
-                      path: subPath,
-                      reason: `Object property not in schema properties and additionalProperties is false`,
-                    });
-                  } else if (s === true) {
-                    result[k] = v;
-                  } else {
-                    result[k] = inner(v, s, subPath);
-                  }
+              if (s === false) {
+                if (!options.removeAdditionalItems) {
+                  throw new ValidationError(`Array item outside of schema items and additionalItems is false`, subPath);
                 }
-                return result;
-              }, {} as any);
-        }
+                filtered.push({
+                  path: subPath,
+                  reason: `Array item outside of schema items and additionalItems is false`,
+                });
+              } else if (i >= maxItems) {
+                if (!options.cropArrays) {
+                  throw new ValidationError(`Array longer than ${maxItems}`, path);
+                }
+
+                filtered.push({
+                  path: subPath,
+                  reason: `Array item index ${i} outside of schema maxItems ${maxItems}`,
+                });
+              } else if (s === true) {
+                result.push(v);
+              } else {
+                result.push(inner(v, s, subPath));
+              }
+              return result;
+            }, [] as any[]);
+          }
+          break;
+
+        case 'object':
+          if (dataType === 'object') {
+            const {properties, additionalProperties = true, required = []} = schema;
+
+            if (!options.ignoreRequiredProperties && required) {
+              for (const k of required) {
+                if (data[k] === undefined) {
+                  throw new ValidationError(`Required object property ${k} missing`, `${path}.${k}`);
+                }
+              }
+            }
+
+            return Object.entries(data)
+                .reduce((result, [k, v]) => {
+                  if (v !== undefined) {
+                    const subPath = `${path}.${k}`;
+                    const s = properties?.[k] ?? additionalProperties;
+
+                    if (s === false) {
+                      if (!options.removeAdditionalProperties) {
+                        throw new ValidationError(`Object property not in schema properties and additionalProperties is false`, subPath);
+                      }
+                      filtered.push({
+                        path: subPath,
+                        reason: `Object property not in schema properties and additionalProperties is false`,
+                      });
+                    } else if (s === true) {
+                      result[k] = v;
+                    } else {
+                      result[k] = inner(v, s, subPath);
+                    }
+                  }
+                  return result;
+                }, {} as any);
+          }
+          break;
+
+          // TODO extract conversions and align enum checks across all primitives
+        case 'number':
+        case 'integer':
+          if (dataType === 'number' || options.convertPrimitives && ['boolean', 'string'].includes(dataType)) {
+            const n = Number(data);
+
+            if (!isNaN(n) && (type === 'number' || Number.isInteger(n))) {
+              return n;
+            }
+          }
+          break;
+
+        case 'boolean':
+          if (dataType === 'boolean') {
+            return data;
+          }
+
+          if (options.convertPrimitives && dataType === 'number') {
+            switch (data) {
+              case 0:
+                return false;
+              case 1:
+                return true;
+              default:
+                break;
+            }
+          }
+
+          if (options.convertPrimitives && dataType === 'string') {
+            switch (data.toLowerCase()) {
+              case 'false':
+                return false;
+              case 'true':
+                return true;
+              default:
+                break;
+            }
+          }
+          break;
+
+        case 'string':
+          if (dataType === 'string') {
+            return data;
+          }
+
+          if (options.convertPrimitives) {
+            return data.toString();
+          }
+          break;
       }
-      throw new ValidationError(`Expected ${types} but found object`, path);
     }
 
-    if (typeof data === 'number') {
-      for (const type of types) {
-        if (type === 'number') {
-          return data;
-        }
+    throw new ValidationError(`Expected ${types} but found ${dataType}`, path);
 
-        if (type === 'integer' && Number.isInteger(data)) {
-          return data;
-        }
-
-        if (options.convertPrimitives && type === 'boolean' && [0, 1].includes(data)) {
-          return data === 1;
-        }
-
-        if (options.convertPrimitives && type === 'string') {
-          return data.toString();
-        }
-      }
-      throw new ValidationError(`Expected ${types} but found number`, path);
-    }
-
-    if (typeof data === 'boolean') {
-      for (const type of types) {
-        if (type === 'boolean') {
-          return data;
-        }
-
-        if (options.convertPrimitives && ['number', 'integer'].includes(type)) {
-          return data ? 1 : 0;
-        }
-
-        if (options.convertPrimitives && type === 'string') {
-          return data.toString();
-        }
-      }
-      throw new ValidationError(`Expected ${types} but found boolean`, path);
-    }
-
-    if (typeof data === 'string') {
-      for (const type of types) {
-        if (type === 'string') {
-          return data;
-        }
-
-        const n = Number(data);
-
-        if (options.convertPrimitives && type === 'integer' && Number.isInteger(n)) {
-          return n;
-        }
-
-        if (options.convertPrimitives && type === 'number' && !isNaN(n)) {
-          return n;
-        }
-
-        const b = data.toLowerCase();
-
-        if (options.convertPrimitives && type === 'boolean' && ['true', 'false'].includes(b))
-          return b === 'true';
-      }
-      throw new ValidationError(`Expected ${types} but found string`, path);
-    }
+    // if (data === null) {
+    //   for (const type of types) {
+    //     if (type === 'null') {
+    //       return data;
+    //     }
+    //   }
+    //   throw new ValidationError(`Expected ${types} but found null`, path);
+    // }
+    //
+    // if (Array.isArray(data)) {
+    //   for (const type of types) {
+    //     if (type === 'array') {
+    //       const {items = true, additionalItems = true, minItems = 0, maxItems = Number.MAX_SAFE_INTEGER} = schema;
+    //
+    //       if (data.length < minItems) {
+    //         throw new ValidationError(`Array shorter than ${minItems}`, path);
+    //       }
+    //
+    //       return (data as any[]).reduce((result, v, i) => {
+    //         const subPath = `${path}[${i}]`;
+    //         const s = Array.isArray(items) ?
+    //             i < items.length ? items[i] : additionalItems :
+    //             items;
+    //
+    //         if (s === false) {
+    //           if (!options.removeAdditionalItems) {
+    //             throw new ValidationError(`Array item outside of schema items and additionalItems is false`, subPath);
+    //           }
+    //           filtered.push({
+    //             path: subPath,
+    //             reason: `Array item outside of schema items and additionalItems is false`,
+    //           });
+    //         } else if (i >= maxItems) {
+    //           if (!options.cropArrays) {
+    //             throw new ValidationError(`Array longer than ${maxItems}`, path);
+    //           }
+    //
+    //           filtered.push({
+    //             path: subPath,
+    //             reason: `Array item index ${i} outside of schema maxItems ${maxItems}`,
+    //           });
+    //         } else if (s === true) {
+    //           result.push(v);
+    //         } else {
+    //           result.push(inner(v, s, subPath));
+    //         }
+    //         return result;
+    //       }, [] as any[]);
+    //     }
+    //   }
+    //
+    //   throw new ValidationError(`Expected ${types} but found array`, path);
+    // }
+    //
+    // if (typeof data === 'object') {
+    //   for (const type of types) {
+    //     if (type === 'object') {
+    //       const {properties, additionalProperties = true, required = []} = schema;
+    //
+    //       if (!options.ignoreRequiredProperties && required) {
+    //         for (const k of required) {
+    //           if (data[k] === undefined) {
+    //             throw new ValidationError(`Required object property ${k} missing`, `${path}.${k}`);
+    //           }
+    //         }
+    //       }
+    //
+    //       return Object.entries(data)
+    //           .reduce((result, [k, v]) => {
+    //             if (v !== undefined) {
+    //               const subPath = `${path}.${k}`;
+    //               const s = properties?.[k] ?? additionalProperties;
+    //
+    //               if (s === false) {
+    //                 if (!options.removeAdditionalProperties) {
+    //                   throw new ValidationError(`Object property not in schema properties and additionalProperties is false`, subPath);
+    //                 }
+    //                 filtered.push({
+    //                   path: subPath,
+    //                   reason: `Object property not in schema properties and additionalProperties is false`,
+    //                 });
+    //               } else if (s === true) {
+    //                 result[k] = v;
+    //               } else {
+    //                 result[k] = inner(v, s, subPath);
+    //               }
+    //             }
+    //             return result;
+    //           }, {} as any);
+    //     }
+    //   }
+    //   throw new ValidationError(`Expected ${types} but found object`, path);
+    // }
+    //
+    // if (typeof data === 'number') {
+    //   for (const type of types) {
+    //     if (type === 'number') {
+    //       return data;
+    //     }
+    //
+    //     if (type === 'integer' && Number.isInteger(data)) {
+    //       return data;
+    //     }
+    //
+    //     if (options.convertPrimitives && type === 'boolean' && [0, 1].includes(data)) {
+    //       return data === 1;
+    //     }
+    //
+    //     if (options.convertPrimitives && type === 'string') {
+    //       return data.toString();
+    //     }
+    //   }
+    //   throw new ValidationError(`Expected ${types} but found number`, path);
+    // }
+    //
+    // if (typeof data === 'boolean') {
+    //   for (const type of types) {
+    //     if (type === 'boolean') {
+    //       return data;
+    //     }
+    //
+    //     if (options.convertPrimitives && ['number', 'integer'].includes(type)) {
+    //       return data ? 1 : 0;
+    //     }
+    //
+    //     if (options.convertPrimitives && type === 'string') {
+    //       return data.toString();
+    //     }
+    //   }
+    //   throw new ValidationError(`Expected ${types} but found boolean`, path);
+    // }
+    //
+    // if (typeof data === 'string') {
+    //   for (const type of types) {
+    //     if (type === 'string') {
+    //       return data;
+    //     }
+    //
+    //     const n = Number(data);
+    //
+    //     if (options.convertPrimitives && type === 'integer' && Number.isInteger(n)) {
+    //       return n;
+    //     }
+    //
+    //     if (options.convertPrimitives && type === 'number' && !isNaN(n)) {
+    //       return n;
+    //     }
+    //
+    //     const b = data.toLowerCase();
+    //
+    //     if (options.convertPrimitives && type === 'boolean' && ['true', 'false'].includes(b))
+    //       return b === 'true';
+    //   }
+    //   throw new ValidationError(`Expected ${types} but found string`, path);
+    // }
   }
 
   return data => {
@@ -217,4 +374,4 @@ const f = filter({
   },
 }, {removeAdditionalProperties: true, convertPrimitives: true});
 
-console.log(f({foo: 'hello', bar: 42, baz: '43'}));
+console.log(f({foo: 'hello', bar: 42, baz: '43', hlall:'woräd'}));
